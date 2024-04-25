7e1993e421ebe6be63e85b5d3a8cfca44779ddea
arthurpessoa <arthurpessoa@outlook.com>
2024-04-25 15:08:19 -0300

    Added apache flink runner and deployments

--- example-portable-flink-runner/src/integrationTest/kotlin/io/github/arthurpessoa/BeamRunnerIntegrationTest.kt
+++ example-portable-flink-runner/src/integrationTest/kotlin/io/github/arthurpessoa/BeamRunnerIntegrationTest.kt
@@ -1,13 +1,13 @@
 package io.github.arthurpessoa
 
 
+import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.Network
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName.parse
-import org.testcontainers.utility.MountableFile.forHostPath
 import kotlin.test.Test
 
 @Testcontainers
@@ -16,25 +16,35 @@ class SparkSubmitIntegrationTest {
     private val network: Network = Network.newNetwork()
 
     @Container
-    val flinkJobManagerContainer: GenericContainer<*> = GenericContainer(parse("apache/flink:latest"))
-
+    val jobManagerContainer: GenericContainer<*> = GenericContainer(parse("apache/flink:latest"))
         .withNetwork(network)
-        .withCommand(
-            "standalone-job",
-            "--job-classname",
-            "io.github.arthurpessoa.AppKt",
-            "--jars",
-            "/home/example-portable-flink-runner-all.jar",
-        )
-        .withCopyFileToContainer(forHostPath("build/libs/.", 365), "/home/")
-        .withCopyFileToContainer(forHostPath("src/integrationTest/resources/flink", 365), "/")
+        .withCommand("jobmanager")
+        .withFileSystemBind("build/libs/", "/jar", BindMode.READ_ONLY)
+        .withNetworkAliases("flink-jobmanager")
         .withLogConsumer { frame ->
             println(frame.utf8String)
         }
 
-    @Test
-    fun `should run in a spark container`() {
+    @Container
+    val taskManagerContainer: GenericContainer<*> = GenericContainer(parse("apache/flink:latest"))
+        .withNetwork(network)
+        .withCommand("taskmanager")
+        .withEnv("FLINK_PROPERTIES", "jobmanager.rpc.address: flink-jobmanager")
+        .dependsOn(jobManagerContainer)
 
-        flinkJobManagerContainer.waitingFor(LogMessageWaitStrategy().withRegEx(".*APP FINISHED.*\\s"))
+    @Test
+    fun `should run in a flink container`() {
+
+        val execInContainer = jobManagerContainer.execInContainer(
+            "/opt/flink/bin/flink", "run",
+            "-m", "flink-jobmanager:8081",
+            "-c", "io.github.arthurpessoa.AppKt",
+            "/jar/example-portable-flink-runner-all.jar"
+        )
+        print(execInContainer.stdout)
+        print(execInContainer.stderr)
+
+        taskManagerContainer.waitingFor(LogMessageWaitStrategy().withRegEx(".*APP FINISHED.*\\s"))
+        println(taskManagerContainer.logs)
     }
 }
